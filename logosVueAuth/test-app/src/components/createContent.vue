3f22d1d4956f97ec510c2235026bb40d401b9637
ecclesao <ecclesao@gmail.com>
2018-07-24 16:40:20 -0700

    fix body error for no punctuation

--- logosVueAuth/test-app/src/components/createContent.vue
+++ logosVueAuth/test-app/src/components/createContent.vue
@@ -97,12 +97,12 @@ require('firebase/auth');
 require('firebase/database');
 
 var config = {
-    apiKey: "AIzaSyDLkhd593mb00bYme_pfqqHGZEFtiC8c4I",
-    authDomain: "web-app-testing-mobiledb.firebaseapp.com",
-    databaseURL: "https://web-app-testing-mobiledb.firebaseio.com",
-    projectId: "web-app-testing-mobiledb",
-    storageBucket: "web-app-testing-mobiledb.appspot.com",
-    messagingSenderId: "461364962866"
+    apiKey: "AIzaSyCA5R7pUDrcpBbhxQ1dk4jLeeZiwl7sV3c",
+    authDomain: "web-app-testing-3309d.firebaseapp.com",
+    databaseURL: "https://web-app-testing-3309d.firebaseio.com",
+    projectId: "web-app-testing-3309d",
+    storageBucket: "web-app-testing-3309d.appspot.com",
+    messagingSenderId: "832434382294"
 };
 
 let app = firebase.initializeApp(config);
