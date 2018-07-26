574ba61fb4ce59f4eb13445e19675572abbcae98
ecclesao <ecclesao@gmail.com>
2018-07-26 10:40:48 -0700

    getting ready for final deploy

--- logosVueAuth/test-app/src/main.js
+++ logosVueAuth/test-app/src/main.js
@@ -31,6 +31,10 @@ var defaultOptions = {
     'span': [ 'href', 'style' ],
     'ul': [ 'data-checked' ]
   },
+  allowedClasses: {
+    'p': [ 'fancy', 'simple', 'ql-indent-1', 'ql-indent-2', 'ql-indent-3', 'ql-indent-4', 'ql-indent-5', 'ql-indent-6', 'ql-indent-7', 'ql-indent-8', 'ql-align-center', 'ql-align-right', 'ql-align-justify' ],
+    'pre': [ 'ql-syntax' ]
+  },
   allowedStyles: {
     '*': {
       // Match HEX and RGB
